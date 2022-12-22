36be2d1bb055eae1a05851c13a1a1849cc73eaa1
xop14 <112329100+xop14@users.noreply.github.com>
2022-12-23 00:03:09 +0900

    Add mobile nav menu

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,60 @@
-const davidGreen = {};
+const cardOverlays = document.querySelectorAll('.card-overlay');
+const navItems = document.querySelector('.nav-items');
+const navProjects = document.querySelector('.nav-projects');
+const navContact = document.querySelector('.nav-contact');
+const mobileNavProjects = document.querySelector('.mobile-nav-projects');
+const mobileNavContact = document.querySelector('.mobile-nav-contact');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerClose = document.querySelector('.hamburger-close');
+const mobileNav = document.querySelector('.mobile-nav');
 
-console.log(davidGreen.portfolio());
+
+let isMenuOpen = false;
+
+
+// smooth scroll nav links
+navProjects.addEventListener("click", () => {
+    document.querySelector("#projects").scrollIntoView({
+        behavior: "smooth"
+    })
+})
+navContact.addEventListener("click", () => {
+    document.querySelector("#contact").scrollIntoView({
+        behavior: "smooth"
+    })
+})
+
+// smooth scroll mobile nav links
+mobileNavProjects.addEventListener("click", () => {
+    document.querySelector("#projects").scrollIntoView({
+        behavior: "smooth"
+    })
+    showMobileNav()
+})
+mobileNavContact.addEventListener("click", () => {
+    document.querySelector("#contact").scrollIntoView({
+        behavior: "smooth"
+    })
+    showMobileNav()
+})
+
+
+// mobile nav
+function showMobileNav() {
+    if (!isMenuOpen) {
+        mobileNav.style.display = 'flex';
+        mobileNav.style.animation = 'slide 0.3s ease-in-out';
+        mobileNav.height = '100vh';
+        isMenuOpen = true;
+    } else {
+        mobileNav.style.animation = 'slide-back 0.3s ease-in-out';
+        setTimeout(() => {
+            mobileNav.style.display = 'none';
+        }, 300);
+        isMenuOpen = false;
+    }
+}
+
+hamburger.addEventListener('click', showMobileNav);
+hamburgerClose.addEventListener('click', showMobileNav);
+
